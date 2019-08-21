fff44759de1054cd6c68dea1b070b6df02ce44c9
權少 <ljqican@163.com>
2019-08-21 21:59:40 +0800

    添加Tree.comptree

--- Program.fs
+++ Program.fs
@@ -17,10 +17,20 @@ module Tree =
         | Branch(_, left, right) -> (1 + max (size left) (size right))
         | Leaf -> 0
 
+    let rec comptree k n =
+        match n with
+        | 0 -> Leaf
+        | _ ->
+            let left = comptree (2 * k) (n - 1)
+            let right = comptree (2 * k + 1) (n - 1)
+            Branch(k, left, right)
+
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
     let tree : Tree<int> = Branch(0, Branch(1, Leaf, Leaf), (Branch(2, Leaf, Branch(3, Leaf, Leaf))))
     printfn "size: %i" (Tree.size tree)
     printfn "depth: %i" (Tree.depth tree)
+    let ct = Tree.comptree 1 5
+    printfn "comptree:%A" ct
     0 // return an integer exit code
