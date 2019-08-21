1a9c3c69bce5e5ef0247cddcdc2ad4afd6ca333c
權少 <ljqican@163.com>
2019-08-21 21:53:55 +0800

    更改Tree的數據位置

--- Program.fs
+++ Program.fs
@@ -3,24 +3,24 @@
 open System
 
 type Tree<'a> =
-    | Branch of Tree<'a> * 'a * Tree<'a>
+    | Branch of 'a * Tree<'a> * Tree<'a>
     | Leaf
 
 module Tree =
     let rec size (tree: Tree<'a>) =
         match tree with
-        | Branch(left, _, right) -> 1 + (size left) + (size right)
+        | Branch(_, left, right) -> 1 + (size left) + (size right)
         | Leaf -> 0
 
     let rec depth (tree: Tree<'a>) =
         match tree with
-        | Branch(left, _, right) -> (1 + max (size left) (size right))
+        | Branch(_, left, right) -> (1 + max (size left) (size right))
         | Leaf -> 0
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
-    let tree : Tree<int> = Branch(Branch(Leaf, 1, Leaf), 0, (Branch(Leaf, 2, Branch(Leaf, 3, Leaf))))
+    let tree : Tree<int> = Branch(0, Branch(1, Leaf, Leaf), (Branch(2, Leaf, Branch(3, Leaf, Leaf))))
     printfn "size: %i" (Tree.size tree)
     printfn "depth: %i" (Tree.depth tree)
     0 // return an integer exit code
