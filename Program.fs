eee8b3131158cab8f4d52942b96f0cfa06f684b6
權少 <ljqican@163.com>
2019-11-10 18:49:44 +0800

    清理代碼

--- Program.fs
+++ Program.fs
@@ -11,40 +11,6 @@ open System.Collections.Generic
 open ImmutableSortedSet
 open ImmutableSortedSetCheck
 
-//type StrComp< =
-//    interface with IComparer
-
-//type ImmutableSortedSetCheck() =
-//    static member list (elements: int list) =
-//        let elements = elements |> List.distinct
-//        elements = (elements |> ImmutableSortedSet.ofList |> ImmutableSortedSet.toList)
-
-//    static member array (elements: int []) =
-//        let elements = elements |> Array.distinct
-//        elements = (elements |> ImmutableSortedSet.ofArray |> ImmutableSortedSet.toArray)
-
-//    static member seq (elements: int list) =
-//        let elements = elements |> Seq.toList |> List.distinct
-//        elements = (elements |> ImmutableSortedSet.ofList |> ImmutableSortedSet.toList)
-
-//    static member map (elements: int list) (mapping: int -> int) =
-//        let a =
-//            elements
-//            |> ImmutableSortedSet.ofList
-//            |> ImmutableSortedSet.map mapping
-//            |> ImmutableSortedSet.toList
-//        let b = elements |> List.map mapping |> List.distinct
-//        a = b
-
-//    static member filter (elements: int list) (predicate: int -> bool) =
-//        let a =
-//            elements
-//            |> ImmutableSortedSet.ofList
-//            |> ImmutableSortedSet.filter predicate
-//            |> ImmutableSortedSet.toList
-//        let b = elements |> List.filter predicate |> List.distinct
-//        a = b
-
 type Comp() =
     interface IComparer<string> with
         member this.Compare(a: string, b: string) =
